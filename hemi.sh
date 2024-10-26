0126c89dbd20ed22e12b753cd78faa3ab8caf803
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2024-10-27 00:33:11 +0530

    Update hemi.sh

--- hemi.sh
+++ hemi.sh
@@ -120,7 +120,7 @@ After=network.target
 WorkingDirectory=/root/hemi/heminetwork_v0.5.0_linux_amd64
 ExecStart=/root/hemi/heminetwork_v0.5.0_linux_amd64/popmd
 Environment="POPM_BTC_PRIVKEY=$private_key"
-Environment="POPM_STATIC_FEE=1000"
+Environment="POPM_STATIC_FEE=1500"
 Environment="POPM_BFG_URL=wss://testnet.rpc.hemi.network/v1/ws/public"
 Restart=on-failure
 
